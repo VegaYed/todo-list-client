1e3a3290fa659b9c44c084bccdd057092f01996c
Cristian Beltran <cbeltranc6@gmail.com>
2020-09-29 13:22:04 -0300

    Estructura componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { CategoriasComponent } from './components/other/categorias/categorias.component';
+import { NuevaTareaComponent } from './components/other/nueva-tarea/nueva-tarea.component';
+import { ListaTareasComponent } from './components/other/lista-tareas/lista-tareas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SidebarComponent,
+    HeaderComponent,
+    CategoriasComponent,
+    NuevaTareaComponent,
+    ListaTareasComponent
   ],
   imports: [
     BrowserModule,
